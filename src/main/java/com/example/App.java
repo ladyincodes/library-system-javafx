e56fec93cafbc81be6bf1f2b63e8187d6c17adc9
Atefeh <111862613+ladyincodes@users.noreply.github.com>
2025-05-19 01:52:38 -0500

    Fixed typo

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("mainscene"));
-        stage.setTitle("Librrary Management System");
+        stage.setTitle("Library Management System");
         stage.setScene(scene);
         stage.show();
     }
